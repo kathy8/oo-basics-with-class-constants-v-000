e4bde8506e6c0af208d54b577457845eaa3a2fec
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-06 16:50:39 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
 GENRES = []
 
 
@@ -14,3 +15,11 @@ GENRES = []
   end
 
 end
+
+def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
